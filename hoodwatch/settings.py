fc6d1fd209decc9f547d377f7cf69df00a4e9f67
Brian-Muchera <mucherabrian2@gmail.com>
2020-11-02 10:46:23 +0300

    created superuser

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,11 +85,12 @@ WSGI_APPLICATION = 'hoodwatch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hood',
+        'USER': 'muchera-brian',
+    'PASSWORD':'sly',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
